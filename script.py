6c68d8e847b3cd82c059efc1492e35dca3fa1286
dulquer9 <73124813+dulquer9@users.noreply.github.com>
2021-03-11 00:06:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,15 +3,7 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
-
-This is Version 2 of Auto Filter Bot
-
-Bot gives button link to files in connected channels on query !
-
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+Sorry.. This Bot Can Only Be Used in <i>Movie Club Group!...</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
@@ -50,17 +42,17 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @MOVIECLUB_CHAT</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Dulquer◢ ◤</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @NickxFury
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>JOIN OUR GROUP :</b> <a href='https://t.me/MOVIECLUB_chat'>Click Here</a> 
 
 """
